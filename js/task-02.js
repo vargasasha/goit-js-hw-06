4b4b8b89a6cb3633c3268103f7e13c4d1c8937b1
Oleksandr Varga <vargasasha@gmail.com>
2023-03-31 22:12:03 +0300

    done task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const array = [];
+
+ingredients.forEach((ingredient) => {
+    // console.log(ingredient);
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    li.classList.add("item");
+    array.push(li);
+});
+
+list.prepend(...array);
