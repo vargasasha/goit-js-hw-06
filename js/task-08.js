136adb1cd4a433a577a48c69fa4f4cecdc4ac651
Oleksandr Varga <vargasasha@gmail.com>
2023-04-07 17:03:38 +0300

    begin task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onSubmit);
 
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    const { email, password } = evt.currentTarget.elements;
+    
+    const user = {
+        email: email.value,
+        password: password.value,
+    };
+
+}
