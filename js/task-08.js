72a6e775973940c5c1a472d054d3c8b66a5cf64d
Oleksandr Varga <vargasasha@gmail.com>
2023-04-07 17:22:29 +0300

    done task-08

--- js/task-08.js
+++ js/task-08.js
@@ -5,10 +5,18 @@ function onSubmit(evt) {
     evt.preventDefault();
 
     const { email, password } = evt.currentTarget.elements;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Всі поля повинні бути заповнені!");
     
+    }
+
     const user = {
         email: email.value,
         password: password.value,
     };
 
+    console.log(user);
+
+    evt.currentTarget.reset()
 }
