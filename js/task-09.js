b815e17e249352ee021271b042b2cbadab2b8b0c
Oleksandr Varga <vargasasha@gmail.com>
2023-04-07 17:46:04 +0300

    done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const button = document.querySelector(".change-color");
+const title = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", changeColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+
+    title.textContent = body.style.backgroundColor;
 }
